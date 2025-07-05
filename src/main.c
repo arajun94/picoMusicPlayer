225b352704e109bd665cc8b986a4ceecd34273e9
arajun <arajyn0904@gmail.com>
2025-07-05 21:45:40 +0900

    コンフリクト解決

--- src/main.c
+++ src/main.c
@@ -17,34 +17,8 @@
 #include "file_stream.h"
 #include "ff.h"
 
-<<<<<<< HEAD
-#define CPU_FREQ 250000000
-
-#define PLAY_BUF_SIZE 512
-uint32_t dma_buf[PLAY_BUF_SIZE];
-
-static PIO pio = pio0;
-static uint sm = 0;
-static int dma_chan;
-
-typedef struct {
-	char riff[4];        // "RIFF"
-	uint32_t size;      // ファイルサイズ - 8
-	char data[4];       // "WAVE"
-} Riff;
-
-typedef struct {
-	uint16_t wFormatTag;          // フォーマットタグ (1: PCM)
-	uint16_t nChannels;           // チャンネル数 (1: モノラル, 2: ステレオ)
-	uint32_t nSamplesPerSec;      // サンプリングレート (例: 44100)
-	uint32_t nAvgBytesPerSec;     // 平均バイト/秒 (サンプリングレート * チャンネル数 * ビット深度 / 8)
-	uint16_t nBlockAlign;         // ブロックアライメント (チャンネル数 * ビット深度 / 8)
-	uint16_t wBitsPerSample;      // ビット深度 (例: 16)
-} WaveFormat;
-=======
 #include "play.h"
 #include "config.h"
->>>>>>> dev
 
 int main()
 {
