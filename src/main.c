0d9d71dc423ef584cd9cddb24105a85dc31158cf
arajun <arajyn0904@gmail.com>
2025-07-20 20:07:40 +0900

    スキップ機能とバッテリー切れ通知

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@
 #include "hardware/clocks.h"
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
+#include "hardware/adc.h"
 
 #include "hw_config.h"
 #include "f_util.h"
@@ -54,18 +55,26 @@ int main()
 	stdio_init_all();
 	set_sys_clock_khz(CPU_FREQ / 1000, true);
 
+    //電源ランプ
     gpio_init(2);
     gpio_set_dir(2, GPIO_OUT);
     gpio_put(2, 1);
-    
+
+    //操作ボタン初期化
     gpio_init(16);
     gpio_init(17);
     gpio_init(18);
-
-    //スタートボタンが押されるまで待機
+    gpio_init(19);
     gpio_set_dir(16, GPIO_IN);
     gpio_set_dir(17, GPIO_IN);
     gpio_set_dir(18, GPIO_IN);
+    gpio_set_dir(19, GPIO_IN);
+
+    adc_init();
+    adc_gpio_init(28);                //ADC0(GPIO26)の端子設定
+    adc_select_input(2);
+
+    //スタートボタンが押されるまで待機
     while (!gpio_get(17));
     while(gpio_get(17));
 
@@ -96,12 +105,15 @@ int main()
 
     uint16_t filesIndex = 1;
     uint16_t filesNum = i;
+    uint8_t tBlink = 0;
+    double vsysVoltage;
 
-    quickSort(files, filesNum);
+    quickSort(&files[1], filesNum);
 
 	play(files[filesIndex]);
 
     while(1){
+        vsysVoltage = (double)adc_read()*3.3*2/4096;
         if(gpio_get(17)){
             if(isPlaying())stop();
             else start();
@@ -121,11 +133,17 @@ int main()
             play(files[filesIndex]);
             while(gpio_get(16));
         }
+        if(gpio_get(19)){
+            skip(10);
+        }
         if(isEnded()){
             play_abort();
             filesIndex++;
             if(filesIndex>=filesNum)filesIndex=1;
             play(files[filesIndex]);
+        }
+        if(vsysVoltage<3.0){
+            gpio_put(2, (tBlink++)%8);
         }
         sleep_ms(100);
     }
